015f66425ffc840ff80fe9d3ed3402b4c03c69f8
Dogpa <dogpa0130@gmail.com>
2022-03-03 08:17:24 +0800

    Initial Commit

---
+++ Swift Practice 137 Tip Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Practice 137 Tip Calculator
+//
+//  Created by Dogpa's MBAir M1 on 2022/3/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Practice 137 Tip Calculator/Swift_Practice_137_Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_137_Tip_CalculatorApp.swift
+//  Swift Practice 137 Tip Calculator
+//
+//  Created by Dogpa's MBAir M1 on 2022/3/3.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_137_Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
